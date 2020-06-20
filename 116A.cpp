dad676be7b8b26c1b261d8c45007f165c0b74632
RASEL KIBRIA <rasel5jkibria@gmail.com>
2020-06-21 01:58:38 +0600

    116A - Tram

---
+++ 116A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,enter,exit;
+    int maxi=0;
+    int passenger=0;
+    int inside;
+    cin>>n;
+    while(n--)
+    {
+        cin>>exit>>enter;
+
+        inside=(passenger-exit)+enter;
+        passenger = inside;
+        if(inside>=maxi)
+        {
+            maxi = inside;
+        }
+    }
+
+    cout<<maxi<<"\n";
+    return 0;
+}

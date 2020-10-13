0a153c63b5eac605df64190365bd7b530fde2aa0
RASEL KIBRIA <rasel5jkibria@gmail.com>
2020-10-13 23:05:30 +0600

    HQ9+

--- 133A.cpp
+++ 133A.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int main(){
-
+    bool flag = false;
     char str[110];
     scanf("%s", str);
 
-    bool flag = false;
     ///HQ9 produce output and + increment value, so we have to check only HQ9
     for(int i=0; i<strlen(str); i++){
         if(str[i]=='H' || str[i]=='Q' || str[i]=='9'){

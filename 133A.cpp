99da825c2c5a33deafb9390f92b64d7dccc8f723
RASEL KIBRIA <rasel5jkibria@gmail.com>
2020-10-13 23:02:18 +0600

    133A.cpp

--- 133A.cpp
+++ 133A.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(){
+
     char str[110];
     scanf("%s", str);
 
-    ///HQ9 produce output and + increment value, so we have to check only HQ9
-
     bool flag = false;
+    ///HQ9 produce output and + increment value, so we have to check only HQ9
     for(int i=0; i<strlen(str); i++){
         if(str[i]=='H' || str[i]=='Q' || str[i]=='9'){
             flag = true;
